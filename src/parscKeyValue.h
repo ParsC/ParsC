255c43da4f080306217dab9f839ba54fbd26f3b9
Jon Wyble <69123330+jwyble@users.noreply.github.com>
2020-12-14 15:18:15 -0500

    Tweaked setName and setValue methods

--- src/parscKeyValue.h
+++ src/parscKeyValue.h
@@ -55,13 +55,13 @@ public:
 	// Construct a key/property with both a name and a value
 	ParsCKeyValue(const char* name, const char* value);
 	virtual ~ParsCKeyValue();
-	void setName(const char* name) {
+	ParsCKeyValue* setName(const char* name) {
 		_name = name;
 	}
 	const char* getName() const {
 		return _name.c_str();
 	}
-	void setValue(const char* value) {
+	ParsCKeyValue* setValue(const char* value) {
 		_value = value;
 	}
 	const char* getValue() const {
