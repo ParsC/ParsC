d093354c8988ce7476e444eb39b34f350fa1d4df
Jon Wyble <69123330+jwyble@users.noreply.github.com>
2021-12-02 16:15:33 -0500

    Updated main header file
    Updated all source files, in compliance to ParsC v2.0

--- src/ParsC.h
+++ src/ParsC.h
@@ -2,7 +2,7 @@
 //
 // ParsC.h
 //
-// Copyright (c) 2019-2020 Jon Wyble
+// Copyright (c) 2019-2021 Jon Wyble
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -41,106 +41,39 @@
 #pragma warning(disable : 4786)
 #endif
 
-class ParsCConfigFile;
-class ParsCSection;
-class ParsCKeyValue;
-class ParsCSectionComment;
-class ParsCData;
-
-/*
-  ParsCBase
-
-  The supreme base for every class included in the ParsC INI configuration file parser.
-  Also binds with the ParsCConfig class.
-*/
-class ParsCBase {
-	friend class ParsCConfig;
-	friend class ParsCConfigFile;
-	friend class ParsCSection;
-public:
-	ParsCBase() : _userData(0) {}
-	virtual ~ParsCBase() {}
-	virtual void print(FILE* file, int depth) const = 0;
-	static void setCondensedWhiteSpace(bool condense) {
-		_condensedWhiteSpace = condense;
-	}
-	static bool isCondensedWhiteSpace() {
-		return _condensedWhiteSpace;
-	}
-	void setUserData(void* user) {
-		_userData = user;
-	}
-	void* getUserData() {
-		return _userData;
-	}
-	const void* getUserData() const {
-		return _userData;
-	}
-	virtual const char* parse(const char* p, ParsCData* data) = 0;
-private:
-	struct Entity {
-		const char* string;
-		unsigned int stringLength;
-		char c;
-	};
-
-	enum {
-		ENTITY_NUMBER = 5,
-		MAXIMUM_ENTITY_LENGTH = 6
-	};
-
-	ParsCBase(const ParsCBase&);
-	void operator=(const ParsCBase& base);
-	static Entity entity[ENTITY_NUMBER];
-	static bool _condensedWhiteSpace;
-protected:
-	static const char* skipWhiteSpace(const char*);
-	inline static bool isWhiteSpace(char ch) {
-		return (isspace((unsigned char)ch) || ch == '\n' || ch == '\r');
-	}
-	inline static bool isWhiteSpace(int ch) {
-		if (ch < 256)
-			return isWhiteSpace((char)ch);
-
-		return false;
-	}
-	static const char* getEntity(const char* in, char* value, int* length);
-	void* _userData;
-};
-
 /*
   ParsCConfig
 
   The parent class for most functions in ParsC. This is what holds all of the elements
   included in INI configuration files in general.
 */
-class ParsCConfig : public ParsCBase {
+class ParsCConfig {
 	friend class ParsCConfigFile;
-	friend class ParsCSection;
 	friend class ParsCKeyValue;
+	friend class ParsCSection;
 public:
 	enum ConfigType {
 		PARSC_CONFIG_FILE,
-		PARSC_SECTION,
-		PARSC_KEY,
+		PARSC_NAME,
 		PARSC_VALUE,
+		PARSC_SECTION,
 		PARSC_SECTION_COMMENT
 	};
 
 	ParsCConfig();
-	~ParsCConfig();
+	virtual ~ParsCConfig();
 	void clear();
-	void setValue(std::string value) {
+	void setValue(const char* value) {
 		_value = value;
 	}
-	std::string getValue() const {
+	const char* getValue() const {
 		return _value;
 	}
-	ParsCConfig* parent() {
-		return _parent;
+	void setUserData(void* userData) {
+		_userData = userData;
 	}
-	const ParsCConfig* parent() const {
-		return _parent;
+	void* getUserData() const {
+		return _userData;
 	}
 	ParsCConfig* sectionName() {
 		return _sectionName;
@@ -158,11 +91,12 @@ private:
 protected:
 	ParsCConfig(ConfigType type);
 	void copyTo(ParsCConfig* target) const;
-	std::string _value;
-	ParsCConfig* _parent;
-	ConfigType _type;
+	const char* _value;
+	ConfigType* _type;
 	ParsCConfig* _sectionName;
 	ParsCConfig* _previous;
 	ParsCConfig* _next;
+	void* _userData;
+	bool _condensedWhiteSpace;
 };
 #endif
